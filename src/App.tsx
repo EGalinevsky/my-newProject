3b670fe74866808644523e79fee13eea62bbb704
Eugene Galinevsky <65033002+EGalinevsky@users.noreply.github.com>
2022-05-10 00:54:06 +0300

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React11111 i do it
-          привет
-
-
-        </a>
-        <div>
-          <h1>
-            ll
-          </h1>
-          how are you
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <div className='loader'>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <div className='ball'></div>
+            <p>loading...</p>
         </div>
       </header>
     </div>
